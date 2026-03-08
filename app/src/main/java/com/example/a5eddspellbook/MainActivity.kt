96d70156fdc5c4ec886cc833cfbcc8b8b1bd814c
deck <svirydovichmaksim@gmail.com>
2026-03-08 18:36:16 +0100

    final final FINAL change

--- app/src/main/java/com/example/a5eddspellbook/MainActivity.kt
+++ app/src/main/java/com/example/a5eddspellbook/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.a5eddspellbook
 
 import android.os.Bundle
 import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -16,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         drawerLayout = findViewById(R.id.drawerLayout)
